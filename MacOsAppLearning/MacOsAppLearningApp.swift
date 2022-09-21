2b6cf1423d2d4e0c572a5a7607165cce40f1961e
HappyVolt <elodiegage@gmail.com>
2022-09-21 09:34:39 +0200

    Testing MacOS app

---
+++ MacOsAppLearning/MacOsAppLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOsAppLearningApp.swift
+//  MacOsAppLearning
+//
+//  Created by Elodie GAGE on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MacOsAppLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
